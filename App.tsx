f1cf3823c638386e5624288a66684089f37cecb2
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2021-11-08 10:34:36 -0300

    First time using styled-components

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Welcome } from "./src/Components/Welcome";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Welcome title="React BareWorkflow com Typescript" />
+      <Dashboard />
     </View>
   );
 }
