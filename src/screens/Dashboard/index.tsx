d6d7b791c1ecb155e93c4ace0bd2c7df2a9588f2
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2021-11-10 14:45:04 -0300

    Header that shows UserInfo

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { Container, Header } from "./styles";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreetings,
+  UserName,
+  Icon,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Header></Header>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/50464472?v=4",
+              }}
+            />
+            <User>
+              <UserGreetings>Olá, </UserGreetings>
+              <UserName>Yuri</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
