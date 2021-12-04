b9d4782bc6a679eda46a6b7a9e9c521e1fa646a1
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2021-12-04 17:35:40 -0300

    Creating TransactionCard Component

---
+++ src/components/TransactionCard/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import {
+  Container,
+  Title,
+  Amount,
+  Footer,
+  Category,
+  Icon,
+  CategoryName,
+  Date,
+} from "./styles";
+
+interface Props {
+  title: string;
+  amount: string;
+}
+
+export function TrasactionCard() {
+  return (
+    <Container>
+      <Title>Desenvolvimento de site</Title>
+
+      <Amount>R$ 12.000,00</Amount>
+
+      <Footer>
+        <Category>
+          <Icon name="dollar-sign" />
+          <CategoryName>Vendas</CategoryName>
+        </Category>
+        <Date>13/04/2020</Date>
+      </Footer>
+    </Container>
+  );
+}
