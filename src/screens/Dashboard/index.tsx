4417ea83d1a92f8f1b5f2c69ee84eb1d2b14e26d
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2021-12-04 16:49:24 -0300

    Header Completed

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HighLightCard } from "../../components/HighlightCard";
 
 import {
   Container,
@@ -10,6 +11,7 @@ import {
   UserGreetings,
   UserName,
   Icon,
+  HighLightCards,
 } from "./styles";
 
 export function Dashboard() {
@@ -31,6 +33,27 @@ export function Dashboard() {
           <Icon name="power" />
         </UserWrapper>
       </Header>
+
+      <HighLightCards>
+        <HighLightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+        />
+        <HighLightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última saída dia 03 de abril"
+        />
+        <HighLightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="01 à 16 de abril"
+        />
+      </HighLightCards>
     </Container>
   );
 }
