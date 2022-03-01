8eeb5e9df8497efdeb7b6a622ec28b528a05a546
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-03-01 19:28:26 -0300

    Starting SignInSocialButton component

---
+++ src/components/SignInSocialButton/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+import { RectButton } from "react-native-gesture-handler";
+
+export const Button = styled(RectButton)``;
+
+export const ImageContainerButton = styled.View``;
+
+export const TextButton = styled.Text``;
