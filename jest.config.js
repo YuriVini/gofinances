c705a885860ab506f7c668976b38425cd0d2a133
Yuri Vinicius <yuri.ybgt.yfo@gmail.com>
2022-06-24 11:16:36 -0300

    Jest configs

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: "jest-expo",
   testEnvironment: "jsdom",
   testPathIgnorePatterns: ["/node_modules", "/android", "/ios"],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect",
+    "jest-styled-components",
+  ],
 };
